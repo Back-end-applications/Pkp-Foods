e6f6b017679eae17e93afe61c993576c27d34912
Vivek <vivek.pyda@gmail.com>
2020-11-09 12:31:46 +0530

    Initial commit

---
+++ Pkp-Foods/src/main/java/com/pkpfoods/web/api/PkpFoodsApplication.java
@@ -0,0 +1,13 @@
+package com.pkpfoods.web.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkpFoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkpFoodsApplication.class, args);
+	}
+
+}
---
+++ Pkp-Foods/src/test/java/com/pkpfoods/web/api/PkpFoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkpfoods.web.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkpFoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
